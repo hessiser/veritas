8ac733de72ad9a84bd0e741b83dc84714c1288f2
NightKoneko <62818210+NightKoneko@users.noreply.github.com>
2025-05-27 10:53:29 -0400

    Fix crashing issue

--- src/kreide/functions.rs
+++ src/kreide/functions.rs
@@ -10,12 +10,112 @@ pub mod rpg {
     use crate::kreide::native_types::*;
     use crate::kreide::gamecore::*;
     use crate::kreide::client::*;
+    pub mod client {
+        use std::{ffi::c_void, sync::LazyLock};
+        use function_name::named;
+        use crate::kreide::native_types::*;
+        use crate::kreide::gamecore::*;
+        use crate::kreide::client::*;
+        #[named]
+        pub unsafe fn TextmapStatic_GetText(
+            a1: *const TextID,
+            a2: *const NativeArray<NativeObject>,
+        ) -> *const NativeString {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const TextID,
+                        *const NativeArray<NativeObject>,
+                    ) -> *const NativeString,
+                >(0x6c98c00 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
+        #[named]
+        pub unsafe fn UIGameEntityUtils_GetAvatarID(a1: *const GameEntity) -> u32 {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const GameEntity) -> u32,
+                >(0x6d9b4a0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+        #[named]
+        pub unsafe fn AvatarModule_GetAvatar(
+            a1: *const c_void,
+            a2: u32,
+        ) -> *const AvatarData {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const c_void, u32) -> *const AvatarData,
+                >(0xcb35760 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
+        #[named]
+        pub unsafe fn AvatarData_get_AvatarName(
+            a1: *const AvatarData,
+        ) -> *const NativeString {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const AvatarData) -> *const NativeString,
+                >(0xcb3ec40 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+    }
     pub mod gamecore {
         use std::{ffi::c_void, sync::LazyLock};
         use function_name::named;
         use crate::kreide::native_types::*;
         use crate::kreide::gamecore::*;
         use crate::kreide::client::*;
+        #[named]
+        pub unsafe fn MonsterRowData_get_CharacterName(
+            a1: *const TextID,
+            a2: *const MonsterRowData,
+        ) -> *const TextID {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const TextID,
+                        *const MonsterRowData,
+                    ) -> *const TextID,
+                >(0x7041470 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
+        #[named]
+        pub unsafe fn MonsterRowData_get_Level(a1: *const MonsterRowData) -> u32 {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const MonsterRowData) -> u32,
+                >(0x703faf0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+        #[named]
+        pub unsafe fn EntityManager__GetEntitySummoner(
+            a1: *const EntityManager,
+            a2: *const GameEntity,
+        ) -> *const GameEntity {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const EntityManager,
+                        *const GameEntity,
+                    ) -> *const GameEntity,
+                >(0x6f9a240 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
         #[named]
         pub unsafe fn TurnBasedAbilityComponent_GetAbilityMappedSkill(
             a1: *const TurnBasedAbilityComponent,
@@ -65,6 +165,30 @@ pub mod rpg {
             }
         }
         #[named]
+        pub unsafe fn AbilityStatic_GetActualOwner(
+            a1: *const GameEntity,
+        ) -> *const GameEntity {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const GameEntity) -> *const GameEntity,
+                >(0x6eafe90 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+        #[named]
+        pub unsafe fn MonsterDataComponent_GetMonsterID(
+            a1: *const MonsterDataComponent,
+        ) -> u32 {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const MonsterDataComponent) -> u32,
+                >(0x703c640 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+        #[named]
         pub unsafe fn GamePlayStatic_GetEntityManager() -> *const EntityManager {
             log::debug!(function_name!());
             unsafe {
@@ -72,6 +196,80 @@ pub mod rpg {
                     usize,
                     unsafe extern "C" fn() -> *const EntityManager,
                 >(0x70012c0 + *crate::GAMEASSEMBLY_HANDLE)()
+            }
+        }
+        #[named]
+        pub unsafe fn AvatarSkillRowData_get_SkillName(
+            a1: *const TextID,
+            a2: *const AvatarSkillRowData,
+        ) -> *const TextID {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const TextID,
+                        *const AvatarSkillRowData,
+                    ) -> *const TextID,
+                >(0x6f3ff10 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
+        #[named]
+        pub unsafe fn AvatarSkillRowData_get_AttackType(
+            a1: *const AvatarSkillRowData,
+        ) -> AttackType {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const AvatarSkillRowData) -> AttackType,
+                >(0x6f3fbe0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
+            }
+        }
+        #[named]
+        pub unsafe fn SkillCharacterComponent_GetSkillData(
+            a1: *const SkillCharacterComponent,
+            a2: i32,
+            a3: i32,
+        ) -> *const SkillData {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const SkillCharacterComponent,
+                        i32,
+                        i32,
+                    ) -> *const SkillData,
+                >(0x7074d20 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2, a3)
+            }
+        }
+        #[named]
+        pub unsafe fn ServantSkillRowData_get_SkillName(
+            a1: *const TextID,
+            a2: *const ServantSkillRowData,
+        ) -> *const TextID {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(
+                        *const TextID,
+                        *const ServantSkillRowData,
+                    ) -> *const TextID,
+                >(0x706ee80 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
+            }
+        }
+        #[named]
+        pub unsafe fn ServantSkillRowData_get_AttackType(
+            a1: *const ServantSkillRowData,
+        ) -> AttackType {
+            log::debug!(function_name!());
+            unsafe {
+                std::mem::transmute::<
+                    usize,
+                    unsafe extern "C" fn(*const ServantSkillRowData) -> AttackType,
+                >(0x706edd0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
             }
         }
         #[named]
@@ -116,204 +314,6 @@ pub mod rpg {
                         *const NativeString,
                     ) -> i32,
                 >(0x115c5210 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn MonsterRowData_get_CharacterName(
-            a1: *const TextID,
-            a2: *const MonsterRowData,
-        ) -> *const TextID {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const TextID,
-                        *const MonsterRowData,
-                    ) -> *const TextID,
-                >(0x7041470 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn MonsterRowData_get_Level(a1: *const MonsterRowData) -> u32 {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const MonsterRowData) -> u32,
-                >(0x703faf0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-        #[named]
-        pub unsafe fn EntityManager__GetEntitySummoner(
-            a1: *const EntityManager,
-            a2: *const GameEntity,
-        ) -> *const GameEntity {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const EntityManager,
-                        *const GameEntity,
-                    ) -> *const GameEntity,
-                >(0x6f9a240 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn MonsterDataComponent_GetMonsterID(
-            a1: *const MonsterDataComponent,
-        ) -> u32 {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const MonsterDataComponent) -> u32,
-                >(0x703c640 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-        #[named]
-        pub unsafe fn AvatarSkillRowData_get_SkillName(
-            a1: *const TextID,
-            a2: *const AvatarSkillRowData,
-        ) -> *const TextID {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const TextID,
-                        *const AvatarSkillRowData,
-                    ) -> *const TextID,
-                >(0x6f3ff10 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn AvatarSkillRowData_get_AttackType(
-            a1: *const AvatarSkillRowData,
-        ) -> AttackType {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const AvatarSkillRowData) -> AttackType,
-                >(0x6f3fbe0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-        #[named]
-        pub unsafe fn ServantSkillRowData_get_SkillName(
-            a1: *const TextID,
-            a2: *const ServantSkillRowData,
-        ) -> *const TextID {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const TextID,
-                        *const ServantSkillRowData,
-                    ) -> *const TextID,
-                >(0x706ee80 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn ServantSkillRowData_get_AttackType(
-            a1: *const ServantSkillRowData,
-        ) -> AttackType {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const ServantSkillRowData) -> AttackType,
-                >(0x706edd0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-        #[named]
-        pub unsafe fn SkillCharacterComponent_GetSkillData(
-            a1: *const SkillCharacterComponent,
-            a2: i32,
-            a3: i32,
-        ) -> *const SkillData {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const SkillCharacterComponent,
-                        i32,
-                        i32,
-                    ) -> *const SkillData,
-                >(0x7074d20 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2, a3)
-            }
-        }
-        #[named]
-        pub unsafe fn AbilityStatic_GetActualOwner(
-            a1: *const GameEntity,
-        ) -> *const GameEntity {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const GameEntity) -> *const GameEntity,
-                >(0x6eafe90 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-    }
-    pub mod client {
-        use std::{ffi::c_void, sync::LazyLock};
-        use function_name::named;
-        use crate::kreide::native_types::*;
-        use crate::kreide::gamecore::*;
-        use crate::kreide::client::*;
-        #[named]
-        pub unsafe fn UIGameEntityUtils_GetAvatarID(a1: *const GameEntity) -> u32 {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const GameEntity) -> u32,
-                >(0x6d9b4a0 + *crate::GAMEASSEMBLY_HANDLE)(a1)
-            }
-        }
-        #[named]
-        pub unsafe fn TextmapStatic_GetText(
-            a1: *const TextID,
-            a2: *const NativeArray<NativeObject>,
-        ) -> *const NativeString {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(
-                        *const TextID,
-                        *const NativeArray<NativeObject>,
-                    ) -> *const NativeString,
-                >(0x6c98c00 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn AvatarModule_GetAvatar(
-            a1: *const c_void,
-            a2: u32,
-        ) -> *const AvatarData {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const c_void, u32) -> *const AvatarData,
-                >(0xcb35760 + *crate::GAMEASSEMBLY_HANDLE)(a1, a2)
-            }
-        }
-        #[named]
-        pub unsafe fn AvatarData_get_AvatarName(
-            a1: *const AvatarData,
-        ) -> *const NativeString {
-            log::debug!(function_name!());
-            unsafe {
-                std::mem::transmute::<
-                    usize,
-                    unsafe extern "C" fn(*const AvatarData) -> *const NativeString,
-                >(0xcb3ec40 + *crate::GAMEASSEMBLY_HANDLE)(a1)
             }
         }
     }
